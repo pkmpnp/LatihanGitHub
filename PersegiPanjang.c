a6649ea559ab8d03865606d0aaf4a8e42d5b9539
pkmpnp <irahmayuni@yahoo.com>
2024-09-07 10:12:53 +0700

    Add files via upload

---
+++ PersegiPanjang.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+int main(){
+  int pj,lbr,luas;
+  printf("Program Luas Persegi Panjang\n");
+  printf("Panjang = ");  scanf("%d",&pj);
+  printf("Lebar = "); scanf("%d",&lbr);
+  luas = pj*lbr;
+  printf("Luas = %d\n",luas);
+  return 0;
+}
